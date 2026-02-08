0c5636997a603efba9c9e1fae9964e326d36e2b3
Aryan Khanna <aryankhanna2004@gmail.com>
2026-02-08 13:44:04 -0700

    rename stack

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -5,7 +5,7 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 
-export class BackendStack extends cdk.Stack {
+export class AIPresentationCoachStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
